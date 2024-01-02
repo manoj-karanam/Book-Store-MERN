44105705be8c2cd9038502ff8d40e865cf74bf14
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-01-02 17:46:33 -0500

    put and post added

--- Backend/index.js
+++ Backend/index.js
@@ -14,13 +14,55 @@ app.listen(PORT,()=>{
     console.log(`App is listening on ${PORT}`)
 })
 
+//display all the books
 app.get('/books',async(req,res)=>{
     try{
         const books= await Book.find({});
-        return res.status(200).json(books)
+        return res.status(200).json({
+            count:books.length,
+            data:books
+        })
     }
-    catch{
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
 
+// get book by id
+app.get('/books/:id',async(req,res)=>{
+    try{
+        const {id} = req.params
+        const book= await Book.findById(id);
+        return res.status(200).json(book)
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+app.put('/books/:id',async(req,res)=>{
+    try{
+        if(!req.body.title||
+            !req.body.author||
+            !req.body.publishyear){
+                return res.status(400).send({
+                    message:'Send all required fields:title,author,publishYear'
+                });
+            }
+        const {id} = req.params
+        const result =await Book.findByIdAndUpdate(id,req.body);
+
+        if (!result){
+            return res.status(404).json({message: 'Book not found'});
+        }
+
+        return res.status(200).send({message:'Book updated successfully'})
+         
+    }
+    catch(error){
+        console.log(error)
     }
 })
 
@@ -38,6 +80,25 @@ app.post('/books',async(req,res)=>{
         }
         const book=await Book.create(newBook);
         return res.status(201).send(book)
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+})
+
+app.delete('/books/:id', async(req,res)=>{
+    try{
+        const {id} = req.params
+
+        const result=await Book.findByIdAndDelete(id);
+
+        if (!result){
+            return res.status(404).json({message: 'Book not found'});
+        }
+
+        return res.status(200).send({message:'Book deleted successfully'})
+    
     }
     catch(error){
         console.log(error.message);
